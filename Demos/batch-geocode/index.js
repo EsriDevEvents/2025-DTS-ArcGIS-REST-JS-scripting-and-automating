12d06af236d12b7b49e9600c3520ce39f03759b2
Mark T <mtorrey@esri.com>
2025-03-04 10:51:51 -0500

    a little more clean up

--- Demos/batch-geocode/index.js
+++ Demos/batch-geocode/index.js
@@ -1,58 +1,53 @@
-import "dotenv/config";
-import { request, ArcGISIdentityManager } from "@esri/arcgis-rest-request";
-import { readFile } from "fs/promises";
-import {
-  createItem,
-  searchItems,
-  removeItem,
-  setItemAccess,
-} from "@esri/arcgis-rest-portal";
-import { exit } from "process";
+import "dotenv/config"
+import { request, ArcGISIdentityManager } from "@esri/arcgis-rest-request"
+import { readFile } from "fs/promises"
+import { createItem, searchItems, removeItem, setItemAccess } from "@esri/arcgis-rest-portal"
+import { exit } from "process"
 
 // create a new authentication manager from the username and password stored in the .env file
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
 
 if (!username || !password) {
-  throw new Error("Username and password are required");
+  throw new Error("Username and password are required")
 }
 
 const authentication = await ArcGISIdentityManager.signIn({
   username,
   password,
-});
+})
 
-const user = await authentication.getUser();
-console.log(`logged in as ${user.username}`);
+const user = await authentication.getUser()
+console.log(`logged in as ${user.username}`)
 
 // check that items with the title "Palm Springs Places" and delete them if they exist
 // this is for this demo to make sure it can be re-run continuously
-console.log("\nChecking for existing items");
+console.log("\nChecking for existing items")
 
 const existingItems = await searchItems({
   q: `title:"Palm Springs Places" AND owner:"${authentication.username}"`,
   authentication,
-});
+})
 
 if (existingItems.results.length > 0) {
   await Promise.all([
     existingItems.results.map(({ id, title, type }) => {
-      console.log(`Deleting ${type} ${id} with title ${title}`);
-      return removeItem({ id, authentication });
+      console.log(`Deleting ${type} ${id} with title ${title}`)
+      return removeItem({ id, authentication })
     }),
-  ]);
+  ])
 
   // wait for 1 second to allow time for the items to be deleted
-  await new Promise((r) => setTimeout(r, 1000));
+  await new Promise(r => setTimeout(r, 1000))
 
-  console.log(`Deleted ${existingItems.results.length} existing items`);
+  console.log(`Deleted ${existingItems.results.length} existing items`)
 } else {
-  console.log("No existing items found");
+  console.log("No existing items found")
 }
 
 // next upload the CSV file and create an item
-const rawData = await readFile("./palm-springs-places.csv");
-const someBlob = new Blob([rawData]);
+const rawData = await readFile("./batch-geocode/palm-springs-places.csv")
+const someBlob = new Blob([rawData])
 
 const item = await createItem({
   item: {
@@ -61,53 +56,54 @@ const item = await createItem({
     file: someBlob,
   },
   authentication,
-});
-console.log("\nCreated item", item.id);
+})
+console.log("\nCreated item", item.id)
 
 // analyze the CSV file to determine the schema and get the publish parameters to publish the service
-const analyzeResult = await request(
-  `${authentication.portal}/content/features/analyze`,
-  {
-    authentication,
-    params: {
-      itemId: item.id,
-      filetype: "csv",
+const analyzeResult = await request(`${authentication.portal}/content/features/analyze`, {
+  authentication,
+  params: {
+    itemId: item.id,
+    filetype: "csv",
+    analyzeParameters: {
+      enableGlobalGeocoding: true,
+      sourceLocale: "en",
+      returnEstimatedRowCount: true,
+      returnSize: true,
+      fieldTypesVersion: "V2",
+      geocodeServiceUrl: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+      sourceCountryHint: "us",
+      sourceCountry: "",
     },
-  }
-);
-console.log("\nItem analyzed", analyzeResult.publishParameters);
+  },
+})
+console.log("\nItem analyzed", analyzeResult.publishParameters)
+analyzeResult.publishParameters.name = `Palm Springs Places_${new Date().getTime()}`
 
-const publishResponse = await request(
-  `${authentication.portal}/content/users/${authentication.username}/publish`,
-  {
-    authentication,
-    params: {
-      itemId: item.id,
-      filetype: "csv",
-      publishParameters: analyzeResult.publishParameters,
-    },
-  }
-);
-console.log("\nItem published", publishResponse);
+const publishResponse = await request(`${authentication.portal}/content/users/${authentication.username}/publish`, {
+  authentication,
+  params: {
+    itemId: item.id,
+    filetype: "csv",
+    publishParameters: analyzeResult.publishParameters,
+  },
+})
+console.log("\nItem published", publishResponse)
 
 if (user.userLicenseTypeId.startsWith("location")) {
-  console.log(
-    "Public sharing not supported for ArcGIS Location Platform accounts"
-  );
-  exit(0);
+  console.log("Public sharing not supported for ArcGIS Location Platform accounts")
+  exit(0)
 }
 
 const sharingResponses = await Promise.all(
-  publishResponse.services.map((service) => {
+  publishResponse.services.map(service => {
     return setItemAccess({
       id: service.serviceItemId,
       access: "public",
       authentication,
-    });
+    })
   })
-);
+)
 
-console.log("\nSharing item publicly", sharingResponses);
-console.log(
-  `View item https://www.arcgis.com/home/item.html?id=${sharingResponses[0].itemId}`
-);
+console.log("\nSharing item publicly", sharingResponses)
+console.log(`View item https://www.arcgis.com/home/item.html?id=${sharingResponses[0].itemId}`)
--- Demos/create-feature-service/index.js
+++ Demos/create-feature-service/index.js
@@ -1,31 +1,63 @@
-import "dotenv/config";
-import { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
+import "dotenv/config"
+import { ArcGISIdentityManager } from "@esri/arcgis-rest-request"
 import {
   createFeatureService,
   addToServiceDefinition,
-} from "@esri/arcgis-rest-feature-service";
+} from "@esri/arcgis-rest-feature-service"
+import { searchItems, removeItem } from "@esri/arcgis-rest-portal"
 
 // Authenticate with an API key
-const accessToken = process.env.ACCESS_TOKEN;
+const accessToken = process.env.ACCESS_TOKEN
+const serviceName = process.env.FEATURE_SERVICE_NAME
 
 if (!accessToken) {
-  throw new Error("An access token is required");
+  throw new Error("An access token is required")
 }
 
 const getIdentity = async () => {
   return await ArcGISIdentityManager.fromToken({
     token: accessToken,
-  });
-};
+  })
+}
+
+const removeItems = async (authentication) => {
+  // check for existing items and delete them if they exist
+  // this is for this demo to make sure it can be re-run continuously
+  console.log("\nChecking for existing items")
+
+  const existingItems = await searchItems({
+    q: `title:${serviceName} AND owner:"${authentication.username}"`,
+    authentication,
+  })
+
+  if (existingItems.results.length > 0) {
+    await Promise.all([
+      existingItems.results.map(({ id, title, type }) => {
+        console.log(`Deleting ${type} ${id} with title ${title}`)
+        return removeItem({ id, authentication })
+      }),
+    ])
+
+    // wait to allow time for the items to be deleted
+    await new Promise((r) => setTimeout(r, 2000))
+
+    console.log(`Deleted ${existingItems.results.length} existing items`)
+  } else {
+    console.log("No existing items found")
+  }
+}
 
 const createNewService = async () => {
   // login to the portal
-  const auth = await getIdentity();
+  console.log("Logging in...")
+  const auth = await getIdentity()
+  console.log(`\tSigned in as ${auth.username}`)
+  await removeItems(auth)
 
   // define layer schema
   const layerSchema = [
     {
-      name: "my_points_ArcGIS_REST_JS",
+      name: serviceName,
       type: "Feature Layer",
       defaultVisibility: true,
       isDataVersioned: false,
@@ -134,13 +166,14 @@ const createNewService = async () => {
       maxRecordCount: 10000,
       capabilities: "Query,Extract",
     },
-  ];
+  ]
 
+  console.log("\nCreating new service...")
   // create the service
   const newService = await createFeatureService({
     authentication: auth,
     item: {
-      name: "DTS 2025 Feature Service",
+      name: serviceName,
       capabilities: "Query, Extract",
       description:
         "Programmatically generated feature service using ArcGIS REST JS",
@@ -154,17 +187,19 @@ const createNewService = async () => {
       },
       spatialReference: { wkid: 4326 },
     },
-  });
+  })
+  console.log("\t")
 
+  console.log("\nAdding schema to new service...")
   // create layer
   const newFeatureLayer = await addToServiceDefinition(newService.serviceurl, {
     authentication: auth,
     layers: layerSchema,
-  });
+  })
 
   console.log(
-    `New item created:\n\tid:${newService.itemId}\n\turl:${newService.serviceurl}`
-  );
-};
+    `New item and service created:\nid: https://www.arcgis.com/home/item.html?id=${newService.itemId}\nurl:${newService.serviceurl}`
+  )
+}
 
-createNewService();
+createNewService()
